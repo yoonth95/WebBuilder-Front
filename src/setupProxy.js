ac029f182b681ca94df55480c4b66ea81eba19b8
윤태현 <78673090+yoonth95@users.noreply.github.com>
2023-12-04 21:57:43 +0900

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:3001',
+      target: 'https://port-0-webbuilder-back-5mk12alp6dco7g.sel5.cloudtype.app/',
       changeOrigin: true,
     }),
   );
