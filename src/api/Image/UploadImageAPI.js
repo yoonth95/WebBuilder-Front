b16506876181e10a00c001a4d9538c030b5c5a51
윤태현 <yoonth0919@gmail.com>
2024-01-08 22:26:16 +0900

    Refactor: 백엔드 엔드포인트로 수정

--- src/api/Image/UploadImageAPI.js
+++ src/api/Image/UploadImageAPI.js
@@ -1,7 +1,7 @@
 export const UploadImageAPI = (formData, setProgress) => {
   return new Promise((resolve, reject) => {
     let xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://localhost:3000/api/uploadImage', true);
+    xhr.open('POST', 'http://13.209.201.210:5000/api/uploadImage', true);
 
     xhr.upload.addEventListener('progress', (e) => {
       if (e.lengthComputable) {
@@ -13,7 +13,7 @@ export const UploadImageAPI = (formData, setProgress) => {
       if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
         const response = JSON.parse(xhr.responseText);
         resolve(response.filePath); // 파일 경로를 resolve 합니다.
-      } else if(xhr.readyState === XMLHttpRequest.DONE) {
+      } else if (xhr.readyState === XMLHttpRequest.DONE) {
         reject(xhr.status); // 오류 발생시 상태 코드를 reject 합니다.
       }
     };
