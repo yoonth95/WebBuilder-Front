ccbe783743e9a684ba92fc86fa204bf48e9c118b
윤태현 <yoonth0919@gmail.com>
2024-01-23 18:31:10 +0900

    Refactor: proxy 변경

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://13.209.201.210:5000',
+      target: process.env.REACT_APP_ENDPOINT_URL,
       changeOrigin: true,
     }),
   );
